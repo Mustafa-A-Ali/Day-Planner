775d5de101a25d82871a918fd7b1fb41c8b37b4a
FlexFocus <mustafamire@gmail.com>
2021-07-09 20:03:52 +0100

    date displayed at the top of the page using moment()

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,11 @@
 
 var now = moment();
 
-console.log(now.format("dddd, MMMM Do YYYY"));
-
-var date = $('#lead');
+var date = $('#currentDay');
 
 date.text(moment().format("MMMM Do YYYY, h:mm a"));
 
-// var todayDate = now.format("dddd, MMMM Do YYYY");
+
 
 
 

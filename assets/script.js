e9e4e7f03acd947a09123334a50176f9e72ca4ae
FlexFocus <mustafamire@gmail.com>
2021-07-09 19:39:00 +0100

    date

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
-var currentDate = document.getElementById("lead")
 
-function generateDate() {
-    currentDate.textContent = moment()
+var now = moment();
 
-}
+console.log(now.format("dddd, MMMM Do YYYY"));
+
+var date = $('#lead');
+
+date.text(moment().format("MMMM Do YYYY, h:mm a"));
+
+// var todayDate = now.format("dddd, MMMM Do YYYY");
+
+
+
